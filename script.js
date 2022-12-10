7ff533a36afa1a0ecd795edd25d8a0b78642ad2b
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2022-12-10 21:57:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ class Calculator {
       if (number === '.' && this.currentOperand.includes('.')) return
       this.currentOperand = this.currentOperand.toString() + number.toString()
     }
+    
+      chooseOperation(operation) {
+      if (this.currentOperand === '') return
+      if (this.previousOperand !== '') {
+        this.compute()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
+    }
