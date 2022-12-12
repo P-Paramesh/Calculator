92a82566960db6413e1ede3a22b8969081f0259a
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2022-12-12 21:24:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -93,3 +93,10 @@ class Calculator {
   const previousOperandTextElement = document.querySelector('[data-previous-operand]')
   const currentOperandTextElement = document.querySelector('[data-current-operand]')
   const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+    numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
