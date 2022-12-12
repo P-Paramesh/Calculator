43f3219f64dced02abfe916dadbb312341987010
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2022-12-12 21:23:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,6 @@ class Calculator {
   const operationButtons = document.querySelectorAll('[data-operation]')
   const equalsButton = document.querySelector('[data-equals]')
   const deleteButton = document.querySelector('[data-delete]')
+  const allClearButton = document.querySelector('[data-all-clear]')
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement = document.querySelector('[data-current-operand]')
