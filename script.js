146ebd8d6cec22edb57143ce6c9a1eff21f87cd1
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2022-12-12 21:24:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,4 @@ class Calculator {
   const allClearButton = document.querySelector('[data-all-clear]')
   const previousOperandTextElement = document.querySelector('[data-previous-operand]')
   const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
