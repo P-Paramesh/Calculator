d705fde3f810dd7dfecd8b963876a1afe7701a1f
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2022-12-24 21:07:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement
       this.currentOperandTextElement = currentOperandTextElement
-     
+      this.clear()
     }
     
         clear() {
