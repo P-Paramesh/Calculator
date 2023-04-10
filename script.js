4d00de1d3073c8ef7399f0062e773dafe19fd117
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-04-10 21:28:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ class Calculator {
       this.previousOperand = ''
       this.operation = undefined
     }
-
       delete() {
       this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
